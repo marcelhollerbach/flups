018e560827a0b15d8ccdfab116d60ae08c8baabf
Marcel Hollerbach <mail@marcel-hollerbach.de>
2018-12-09 13:34:01 +0100

    a better config

--- flups/config.py
+++ flups/config.py
@@ -1,10 +1,15 @@
+import configparser
+
 class Config:
   def __init__(self):
-    self.repository_path = "path-to-repository"
-    self.repository_patch_origin = "where-master-is-coming-from"
-    self.repository_patch_destination = "where-patches-are-going"
-    self.ssh_pubkey = "<path-to-pubkey>"
-    self.ssh_privkey = "<path-to-privkey>"
-    self.ssh_passphrase = 'passphrase to decrypt'
-    self.phabricator_url = 'phabricator-url/api/ (THE API PART IS IMPORTANT)'
-    self.phabricator_token = "conditor-token"
+    config = configparser.ConfigParser()
+    config.read('example.ini')
+
+    self.repository_path = config["repository"]["path"]
+    self.repository_patch_origin = config["repository"]["patch-origin"]
+    self.repository_patch_destination = config["repository"]["patch-destination"]
+    self.ssh_pubkey = config["git-ssh-connection"]["pub-key"]
+    self.ssh_privkey = config["git-ssh-connection"]["priv-key"]
+    self.ssh_passphrase = config["git-ssh-connection"]["passphrase"]
+    self.phabricator_url = config["phabricator"]["url"]
+    self.phabricator_token = config["phabricator"]["conduit-token"]
